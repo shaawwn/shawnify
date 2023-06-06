6a4dd6fc6187335a3bba116524206973247cebf9
Shawn Lentz <shawn.lentz@gmail.com>
2023-06-06 10:46:11 -1000

    renamed a new carousel files

---
+++ src/components/_PlaylistCover.js
@@ -0,0 +1,12 @@
+// alt to the link cards
+
+function PlaylistCover(props) {
+
+    return(
+        <div className="playlist-cover">
+            <h1>I'm a playlist cover!</h1>
+        </div>
+    )
+}
+
+export default PlaylistCover
