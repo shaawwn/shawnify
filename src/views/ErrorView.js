d4dea886b0cc0c228c7e9264a58bcc98d3852897
Shawn Lentz <shawn.lentz@gmail.com>
2023-06-08 16:39:31 -1000

    actually added the error page

---
+++ src/views/ErrorView.js
@@ -0,0 +1,12 @@
+import {useState, useEffect} from 'react';
+
+function ErrorView(props) {
+
+    return(
+        <div className="error-view">
+            <h1>Only users with premium accounts can use App :( But its Spotify is pretty worth it!</h1>
+        </div>
+    )
+}
+
+export default ErrorView
